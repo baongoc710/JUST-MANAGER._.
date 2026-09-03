cf08861afebeebe84a28dd5943a34fe13a649d72
Bao Ngoc <kaythlynjohn@gmail.com>
2026-09-03 13:39:32 +0700

    Initial commit: Just manager v0.1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+int main(){
+    string course_name;
+    string course_code;
+    string platform;
+    string instructor;
+    string status;
+    string start_date;
+    string end_date;
+    double expected_score;
+
+    cout << "Course name: " << course_name << endl;
+    cout << "Course code: " << course_code << endl;
+    cout << "Platform: " << platform << endl;
+    cout << "Instructor: " << instructor << endl;
+    cout << "Status: " << status << endl;
+    cout << " Start date: " << start_date << endl;
+    cout << "End date: " << end_date << endl;
+    cout << "Expected score: " << expected_score << endl;
+
+return 0;
+}
